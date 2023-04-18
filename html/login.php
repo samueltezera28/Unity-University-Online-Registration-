55b60b0bd613ea2c8f45455bccf5c518abcb8f7a
samueltezera28 <samueltezera28@gmail.com>
2023-04-19 00:01:00 +0300

    temp

---
+++ html/login.php
@@ -0,0 +1,20 @@
+<?php
+$conn = "";
+
+$conn = mysqli_connect("localhost", "root", "", "student");
+
+if (isset($_POST['submit'])) {
+    $username = filter_input(INPUT_POST, "username", FILTER_SANITIZE_SPECIAL_CHARS);
+    $password = filter_input(INPUT_POST, "password", FILTER_SANITIZE_SPECIAL_CHARS);
+    // $username = $_POST['username'];
+    // $password = $_POST['password'];
+    $sql = mysqli_query($conn, "select HashedPassword from student_login where username='$username'");
+    if ($row = mysqli_fetch_array($sql)) {
+        if ($password == $row['HashedPassword']) {
+            header("location:../dashboard/index.htm");
+            exit();
+        } else
+            echo "invalid Password";
+    } else
+        echo "User Not found";
+}
