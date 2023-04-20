582afc3aff6c0b6c75a732585664a0fd83dad2da
samueltezera28 <samueltezera28@gmail.com>
2023-04-20 14:42:35 +0300

    error msg fixed

--- html/login.php
+++ html/login.php
@@ -1,3 +1,54 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../css/index.css">
+    <title>Document</title>
+</head>
+
+<body>
+    <div class="container">
+        <div class="main-nav">
+            <img src="/img/Unity_University_logo.png" alt="" class="logo">
+            <ul class="main-menu">
+                <li><a href="../index.html">Home</a></li>
+                <li><a href="#">Apply</a></li>
+                <li><a href="#">Alumni</a></li>
+                <li><a href="./html/login.html">Login</a></li>
+            </ul>
+        </div>
+    </div>
+
+    <div class="login">
+        <div class="login-container">
+            <form action="login.php" method="post" id="login" class="form">
+                <h1>Student Login</h1>
+                <div class="form-field">
+                    <label for="username">Username:</label>
+                    <input type="text" name="username" id="username" autocomplete="off">
+                    <small></small>
+                </div>
+                <div class="form-field">
+                    <label for="password">Password:</label>
+                    <input type="password" name="password" id="password" autocomplete="off">
+                    <small id="error-msg"></small>
+                </div>
+                <div class="form-field">
+                    <input type="submit" value="Log in" id="sub" name="submit">
+                </div>
+            </form>
+        </div>
+
+    </div>
+
+    <!-- <script src="../script/login.js"></script> -->
+</body>
+
+</html>
+
 <?php
 $conn = "";
 
@@ -14,7 +65,7 @@ if (isset($_POST['submit'])) {
             header("location:../dashboard/index.php");
             exit();
         } else
-            echo "invalid Password";
+            echo '<script>document.getElementById("error-msg").innerHTML = "Invalid Password";</script>';
     } else
-        echo "User Not found";
+        echo '<script>document.getElementById("error-msg").innerHTML = "User Not Found";</script>';
 }
