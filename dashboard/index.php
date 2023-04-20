c4fba40e3c8af880a5e737a09994cb3c13b58d96
samueltezera28 <samueltezera28@gmail.com>
2023-04-20 17:38:48 +0300

    font awesome added for the dashboard

--- dashboard/index.php
+++ dashboard/index.php
@@ -3,6 +3,7 @@
 
 <head>
     <link rel="stylesheet" href="./css/index.css">
+    <link rel="stylesheet" href="../font-awesome-4.7.0/css/font-awesome.min.css">
     <title>Student Dashboard</title>
 
 </head>
@@ -14,12 +15,12 @@
     <div class="container-wrapper">
         <div class="nav">
             <ul>
-                <li><a href="#" onclick="showHome()">Home</a></li>
-                <li><a href="#" onclick="showPlanning()">My Planning</a></li>
-                <li><a href="#" onclick="showCourse()">Course</a></li>
-                <li><a href="#" onclick="showBooks()">Books/Modules</a></li>
-                <li><a href="#" onclick="showGrade()">Grade</a></li>
-                <li><a href="#" onclick="showProfile()">My Profile</a></li>
+                <li><a href="#" onclick="showHome() "><i class="fa fa-home"></i>Home</a></li>
+                <li><a href="#" onclick="showPlanning()"><i class="fa fa-calendar"></i>My Planning</a></li>
+                <li><a href="#" onclick="showCourse()"><i class="fa fa-graduation-cap"></i> Course</a></li>
+                <li><a href="#" onclick="showBooks()"><i class="fa fa-book"></i>Books/Modules</a></li>
+                <li><a href="#" onclick="showGrade()"><i class="fa fa-calculator"></i>Grade</a></li>
+                <li><a href="#" onclick="showProfile()"><i class="fa fa-user"></i>My Profile</a></li>
             </ul>
         </div>
         <div id="container">
