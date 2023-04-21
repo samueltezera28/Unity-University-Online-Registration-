9ed2ea2d45fa040c3db3ec345950ba02f5f776cf
samueltezera28 <samueltezera28@gmail.com>
2023-04-21 13:30:26 +0300

    😒:static schedule

---
+++ dashboard/planning.php
@@ -0,0 +1,131 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Student Schedule</title>
+</head>
+
+<body>
+    <table>
+        <tr>
+            <th>Monday</th>
+            <th>Tuesday</th>
+            <th>Wednesday</th>
+            <th>Thursday</th>
+            <th>Friday</th>
+            <th>Saturday</th>
+            <th>Sunday</th>
+        </tr>
+        <tr>
+            <td>
+                <div class="date">Jan 1</div>
+                OOP<br>
+                9:00 AM - 10:30 AM
+            </td>
+            <td>
+                <div class="date">Jan 2</div>
+                C++<br>
+                11:00 AM - 12:30 PM
+            </td>
+            <td>
+                <div class="date">Jan 3</div>
+                Java<br>
+                8:00 AM - 9:30 AM
+            </td>
+            <td>
+                <div class="date">Jan 4</div>
+                Datastructure<br>
+                10:00 AM - 11:30 AM
+            </td>
+            <td>
+                <div class="date">Jan 5</div>
+                algorithm<br>
+                1:00 PM - 2:30 PM
+            </td>
+            <td>
+                <div class="date">Jan 6</div>
+                MATH<br>
+                2:00 PM - 3:30 PM
+            </td>
+            <td>
+                <div class="date">Jan 7</div>
+                <span style="color: #ccc;">No Class</span>
+            </td>
+        </tr>
+        <tr>
+            <td>
+                <div class="date">Jan 1</div>
+                OOP<br>
+                9:00 AM - 10:30 AM
+            </td>
+            <td>
+                <div class="date">Jan 2</div>
+                C++<br>
+                11:00 AM - 12:30 PM
+            </td>
+            <td>
+                <div class="date">Jan 3</div>
+                Java<br>
+                8:00 AM - 9:30 AM
+            </td>
+            <td>
+                <div class="date">Jan 4</div>
+                Datastructure<br>
+                10:00 AM - 11:30 AM
+            </td>
+            <td>
+                <div class="date">Jan 5</div>
+                algorithm<br>
+                1:00 PM - 2:30 PM
+            </td>
+            <td>
+                <div class="date">Jan 6</div>
+                MATH<br>
+                2:00 PM - 3:30 PM
+            </td>
+            <td>
+                <div class="date">Jan 7</div>
+                <span style="color: #ccc;">No Class</span>
+            </td>
+        </tr>
+        <tr>
+            <td>
+                <div class="date">Jan 1</div>
+                OOP<br>
+                9:00 AM - 10:30 AM
+            </td>
+            <td>
+                <div class="date">Jan 2</div>
+                C++<br>
+                11:00 AM - 12:30 PM
+            </td>
+            <td>
+                <div class="date">Jan 3</div>
+                Java<br>
+                8:00 AM - 9:30 AM
+            </td>
+            <td>
+                <div class="date">Jan 4</div>
+                Datastructure<br>
+                10:00 AM - 11:30 AM
+            </td>
+            <td>
+                <div class="date">Jan 5</div>
+                algorithm<br>
+                1:00 PM - 2:30 PM
+            </td>
+            <td>
+                <div class="date">Jan 6</div>
+                MATH<br>
+                2:00 PM - 3:30 PM
+            </td>
+            <td>
+                <div class="date">Jan 7</div>
+                <span style="color: #ccc;">No Class</span>
+            </td>
+        </tr>
+
+    </table>
+</body>
+
+</html>
