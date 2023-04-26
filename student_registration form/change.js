cd5b4fb2b77888167395891571f17c404ad893b8
sifamelon <sifensolomon7488@gmail.com>
2023-04-26 08:18:19 +0300

    first stude_reg

---
+++ student_registration form/change.js
@@ -0,0 +1,12 @@
+const form = document.querySelector("form"),
+  nextbtn = form.querySelector(".btn"),
+  backbtn = form.querySelector(".bbtn"),
+  allInput = form.querySelectorAll(".first input");
+
+nextbtn.addEventListener("click", (e) => {
+  e.preventDefault();
+  form.classList.add("setActive");
+});
+backbtn.addEventListener("click", () => {
+  form.classList.remove("setActive");
+});
---
+++ student_registration form/reg_Stud.php
@@ -0,0 +1,167 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+    <div class="contaner">
+        <h3>STUDENT SUBMISSION FORM</h3>
+        <form action="" method="post">
+            <div class="form first">
+                <div class="personal-details">
+                    <span class="info">Student Information</span>
+                    <div class="form-field">
+                        <div class="inpit-field">
+                            <label for="">First Name</label>
+                            <input type="text" placeholder="First Name">
+                        </div>
+                        <div class="inpit-field">
+                            <label for="">Lasr Name</label>
+                            <input type="text" placeholder="First Name">
+                        </div>
+
+                        <div class="inpit-field">
+                            <label for="">Email</label>
+                            <input type="Email" placeholder="First Name">
+                        </div>
+
+                        <div class="inpit-field">
+                            <label for="">BORN</label>
+                            <input type="date" placeholder="First Name">
+                        </div>
+                        <div class="inpit-field">
+                            <label for="">GENDER</label>
+                            <select>
+                                <option disabled selected>Select gender</option>
+                                <option>Male</option>
+                                <option>Female</option>
+                                <option>Others</option>
+                            </select>
+                        </div>
+                        <div class="inpit-field">
+                            <label for="">Mobile Number</label>
+                            <input type="Email" placeholder="First Name">
+                        </div>
+                    </div>
+                </div>
+                <div class="personal-details">
+                    <span class="info">Student Adress</span>
+                    <div class="form-field">
+                        <div class="inpit-field">
+                            <label for="">PO.BOX</label>
+                            <input type="text" placeholder="First Name">
+                        </div>
+                        <div class="inpit-field">
+                            <label for="">Natinality</label>
+                            <input type="text" placeholder="First Name">
+                        </div>
+                        <div class="inpit-field">
+                            <label for="">Emergency</label>
+                            <input type="text" placeholder="First Name">
+                        </div>
+                        <div class="inpit-field">
+                            <label for="">zone,keble</label>
+                            <input type="text" placeholder="First Name">
+                        </div>
+                        <div class="inpit-field">
+                            <label for="">Were did you come from</label>
+                            <select>
+                                <option disabled selected>Select place</option>
+                                <option>Rular</option>
+                                <option>Urban</option>
+                            </select>
+                        </div>
+                        <div class="inpit-field">
+                            <label for="">Birth Town</label>
+                            <input type="text" placeholder="First Name">
+                        </div>
+                    </div>
+                    <button class="btn">
+                        <span> Next</span>
+                    </button>
+                </div>
+            </div>
+            <!-- second form -->
+            <div class="form second">
+                <div class="adress">
+                    <span class="info">personal address</span>
+                    <div class="form-field">
+                        <div class="inpit-field">
+                            <label for="">First Name</label>
+                            <input type="text" placeholder="First Name">
+                        </div>
+                        <div class="inpit-field">
+                            <label for="">DATE</label>
+                            <input type="date" placeholder="First Name">
+                        </div>
+                        <div class="inpit-field">
+                            <label for="">Email</label>
+                            <input type="Email" placeholder="First Name">
+                        </div>
+                        <div class="inpit-field">
+                            <label for="">First Name</label>
+                            <input type="text" placeholder="First Name">
+                        </div>
+                        <div class="inpit-field">
+                            <label for="">DATE</label>
+                            <input type="date" placeholder="First Name">
+                        </div>
+                        <div class="inpit-field">
+                            <label for="">Email</label>
+                            <input type="Email" placeholder="First Name">
+                        </div>
+                    </div>
+                </div>
+
+                <div class="personal-details">
+                    <span class="info">basic info</span>
+                    <div class="form-field">
+                        <div class="inpit-field">
+                            <label for="">First Name</label>
+                            <input type="text" placeholder="First Name">
+                        </div>
+                        <div class="inpit-field">
+                            <label for="">DATE</label>
+                            <input type="date" placeholder="First Name">
+                        </div>
+                        <div class="inpit-field">
+                            <label for="">Email</label>
+                            <input type="Email" placeholder="First Name">
+                        </div>
+                        <div class="inpit-field">
+                            <label for="">First Name</label>
+                            <input type="text" placeholder="First Name">
+                        </div>
+                        <div class="inpit-field">
+                            <label for="">DATE</label>
+                            <input type="date" placeholder="First Name">
+                        </div>
+                        <div class="inpit-field">
+                            <label for="">Email</label>
+                            <input type="Email" placeholder="First Name">
+                        </div>
+                    </div>
+                    <div class="buttons">
+                        <div class="bbtn">
+                            <span class="btnText">back</span>
+                        </div>
+                        <button class="btn">
+                            <span class="btnText">Submit</span>
+                        </button>
+                    </div>
+
+                </div>
+            </div>
+        </form>
+    </div>
+    <div>
+        <script src="change.js"></script>
+</body>
+
+</html>
