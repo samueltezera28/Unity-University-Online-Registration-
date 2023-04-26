bd6b85f11f0fcfbeb7f78d48ee932fee9a2d912f
samueltezera28 <samueltezera28@gmail.com>
2023-04-26 11:35:40 +0300

    database easy connect

--- database.php
+++ database.php
@@ -1,14 +1,28 @@
 <?php
+// $db_server = "localhost";
+// $db_user = "root";
+// $db_pass = "";
+// $db_name = "student";
+// $conn = "";
+
+// $conn = mysqli_connect($db_server, $db_user, $db_pass, $db_name);
+
+// if ($conn) {
+//     echo "you are connected";
+// } else {
+//     echo "could not connect";
+// }
+// Step 1: Establish database connection
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "student";
 $db_server = "localhost";
-$db_user = "root";
-$db_pass = "";
-$db_name = "student";
-$conn = "";
-
-$conn = mysqli_connect($db_server, $db_user, $db_pass, $db_name);
-
+$conn = mysqli_connect($servername, $username, $password, $dbname);
+// $conn = new mysqli($servername, $username, $password, $dbname);
 if ($conn) {
     echo "you are connected";
-} else {
-    echo "could not connect";
+}
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
 }
