a9027c156fc58be5ce9b1d4e6d540a6b0cf55554
samueltezera28 <samueltezera28@gmail.com>
2023-06-02 15:08:49 +0300

    modify

--- dashboard/database.php
+++ dashboard/database.php
@@ -1,13 +1,13 @@
-<?php 
+<?php
 
 $servername = "localhost";
-        $username = "root";
-        $password = "";
-        $dbname = "unity_university";
-        $db_server = "localhost";
-        $conn = mysqli_connect($servername, $username, $password, $dbname);
-        // $conn = new mysqli($servername, $username, $password, $dbname);
+$username = "root";
+$password = "";
+$dbname = "unity_university";
+$db_server = "localhost";
+$conn = mysqli_connect($servername, $username, $password, $dbname);
+//$conn = new mysqli($servername, $username, $password, $dbname);
 
-        if ($conn->connect_error) {
-            die("Connection failed: " . $conn->connect_error);
-        }
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
