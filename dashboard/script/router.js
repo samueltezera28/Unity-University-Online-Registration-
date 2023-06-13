43d43f92b9972ae50292919c4c1162a28b96679a
samueltezera28 <samueltezera28@gmail.com>
2023-06-13 10:44:14 +0300

    modification

--- dashboard/script/router.js
+++ dashboard/script/router.js
@@ -133,3 +133,67 @@ function showProfile() {
     xhttp.send();
    // document.getElementById("container").innerHTML = "<h2>My Profile</h2><p>Here you can view and edit your profile.</p>";
 }
+
+
+
+function calculateGPA() {
+    var table = document.getElementById("gpa-table-body");
+    var rows = table.getElementsByTagName("tr");
+    var totalCreditHours = 0;
+    var totalGradePoints = 0;
+
+    for (var i = 0; i < rows.length; i++) {
+        var creditHoursInput = rows[i].querySelector(".credit-hours");
+        var gradeInput = rows[i].querySelector(".grade");
+
+
+        var creditHours = parseFloat(creditHoursInput.value);
+        var grade = gradeInput.value;
+        console.log(creditHours);
+        console.log(grade);
+
+        if (!isNaN(creditHours)) {
+            totalCreditHours += creditHours;
+            totalGradePoints += calculateGradePoints(grade) * creditHours;
+        }
+    }
+
+    var gpa = totalGradePoints / totalCreditHours;
+    console.log(gpa)
+    if (isNaN(gpa) || !isFinite(gpa)) {
+        document.getElementById("gpa").innerHTML = "-";
+    } else {
+        document.getElementById("gpa").innerHTML = gpa.toFixed(2);
+    }
+}
+
+function calculateGradePoints(grade) {
+    switch (grade) {
+        case "A+":
+            return 4.0;
+        case "A":
+            return 4.0;
+        case "A-":
+            return 3.7;
+        case "B+":
+            return 3.3;
+        case "B":
+            return 3.0;
+        case "B-":
+            return 2.7;
+        case "C+":
+            return 2.3;
+        case "C":
+            return 2.0;
+        case "C-":
+            return 1.7;
+        case "D+":
+            return 1.3;
+        case "D":
+            return 1.0;
+        case "F":
+            return 0.0;
+        default:
+            return 0.0;
+    }
+}
