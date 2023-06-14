c489637bd647707894652373ab926375a9fde868
sifamelon <sifensolomon7488@gmail.com>
2023-06-14 14:47:34 +0300

    some fix

--- dashboard/database.php
+++ dashboard/database.php
@@ -7,7 +7,6 @@ $dbname = "unity_university";
 $db_server = "localhost";
 $conn = mysqli_connect($servername, $username, $password, $dbname);
 //$conn = new mysqli($servername, $username, $password, $dbname);
-
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
 }
