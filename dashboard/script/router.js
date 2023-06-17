9c768071f78db7cacc6d6ff8edaebfa8a539494e
samueltezera28 <samueltezera28@gmail.com>
2023-06-17 23:12:51 +0300

    show events

--- dashboard/script/router.js
+++ dashboard/script/router.js
@@ -134,6 +134,28 @@ function showProfile() {
    // document.getElementById("container").innerHTML = "<h2>My Profile</h2><p>Here you can view and edit your profile.</p>";
 }
 
+function showevents() {
+    var container = document.querySelector('.container');
+
+    // Create an instance of XMLHttpRequest object
+    var xhttp = new XMLHttpRequest();
+
+    // Define a callback function to be executed when the AJAX request is complete
+    xhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            // Insert the contents of the fetched HTML file into the container element
+            container.innerHTML = this.responseText;
+        }
+    };
+
+    // Open the AJAX request and specify the URL of the HTML file to fetch
+    xhttp.open("GET", "./temp.html", true);
+
+    // Send the AJAX request
+    xhttp.send();
+   // document.getElementById("container").innerHTML = "<h2>My Profile</h2><p>Here you can view and edit your profile.</p>";
+}
+
 
 
 function calculateGPA() {
