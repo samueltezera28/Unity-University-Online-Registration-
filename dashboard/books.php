23aeebe4333d3916b34fdfcfd49669efb080c39c
samueltezera28 <samueltezera28@gmail.com>
2023-06-18 18:19:15 +0300

    book updated but search is not working 😢

--- dashboard/books.php
+++ dashboard/books.php
@@ -11,10 +11,10 @@ include("database.php");
 </head>
 
 <body>
-    <h1>Student Dashboard</h1>
+    <h1>Once you learn to read you will be forever free. -Frederick Douglas</h1>
 
     <div class="search-bar">
-        <form action="./course.php" method="post">
+        <form action="./books.php" method="post">
             <input type="text" name="book" id="search-input" placeholder="Search by title or instructor name">
             <button type="submit" name="save">Search</button>
         </form>
@@ -48,7 +48,8 @@ include("database.php");
     // $conn = mysqli_connect("localhost","root","","student");
     if (count($_POST) > 0) {
         $book = $_POST['book'];
-        $result = mysqli_query($conn, "SELECT * FROM book_download where title='$book' ");
+        echo "{$book}.heyyyyy";
+        $result = mysqli_query($conn, "SELECT * FROM book_download where title like'$book%' ");
         if ($result->num_rows > 0) {
             echo "<ul class=\"resource-list\">";
             while ($row = $result->fetch_assoc()) {
