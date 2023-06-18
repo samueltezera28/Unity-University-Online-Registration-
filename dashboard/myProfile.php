ce65e1007c15f3bb0dafda8571217584e4b868ac
samueltezera28 <samueltezera28@gmail.com>
2023-06-18 18:21:24 +0300

    batch year updated

--- dashboard/myProfile.php
+++ dashboard/myProfile.php
@@ -11,8 +11,28 @@ $sql = "SELECT Fname,Lname,department.Department_code,department.Department_name
         where student_data.S_id={$_SESSION['student_id']} ";
 $result = $conn->query($sql);
 $row = $result->fetch_assoc();
+$batchyear = $row['entry'];
+$yearnumber = date('Y', strtotime($batchyear));
+$currentyear = date("Y");
+$finalYear = $currentyear - $yearnumber;
+
+function batch_cal($finalYear)
+{
+    if ($finalYear == 0) {
+        echo "1st Year";
+    } else if ($finalYear == 2) {
+        echo $finalYear . "nd Year";
+    } else if ($finalYear == 3) {
+        echo $finalYear . "rd Year";
+    } else if ($finalYear == 4) {
+        echo $finalYear . "th Year";
+    }
+}
+
 $dcode = $row['Department_code'];
 $_SESSION['dcode'] = "{$dcode}";
+
+
 ?>
 
 <!DOCTYPE html>
@@ -43,7 +63,7 @@ $_SESSION['dcode'] = "{$dcode}";
                                         echo ucwords($row['Lname']); ?></p>
             <p><span> ID Number:</span> <?php echo $_SESSION['student_id'] ?></p>
             <p><span> Department: </span><?php echo ucwords($row['Department_name']); ?> </p>
-            <p><span> Batch Year: </span><?php echo $row['entry'] ?></p>
+            <p><span> Batch Year: </span><?php batch_cal($finalYear) ?></p>
         </div>
     </div>
 
